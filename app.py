a583b8170396eab2c3476fa6f107e72984edab49
Collins Mutai <collinsfrontend@gmail.com>
2021-03-07 05:49:12 +0300

    connect to database

--- app.py
+++ app.py
@@ -1,51 +1,107 @@
-# import os
-# from flask import Flask, request, abort, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
-
-# def create_app(test_config=None):
-#   # create and configure the app
-#   app = Flask(__name__)
-#   CORS(app)
-
-#   return app
-
-# APP = create_app()
-
-# if __name__ == '__main__':
-#     APP.run(host='0.0.0.0', port=8080, debug=True)
+from flask import Flask, request, abort, jsonify
+from models import setup_db, Actor
+from flask_cors import CORS
 
 
-import os
-from flask import Flask, jsonify
+# Pagination
 
-# from models import setup_db
+ACTORS_PER_SHELF = 0
+
+
+def paginate_actors(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * ACTORS_PER_SHELF
+    end = start + ACTORS_PER_SHELF
+
+    actors = [actor.format() for actor in selection]
+    current_actors = actors[start:end]
+
+    return current_actors
+
+
+# App Config.
 
 
 def create_app(test_config=None):
-
     app = Flask(__name__)
-    # setup_db(app)
-    # CORS(app)
+    setup_db(app)
+    CORS(app)
 
-    @app.route("/")
-    def get_greeting():
-        # excited = os.environ['EXCITED']
-        # greeting = "Hello"
-        # if excited == 'true': greeting = greeting + "!!!!!"
-        # return greeting
-        return jsonify({"Greeting": "Hello!"})
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
-    # @app.route("/coolkids")
-    # def be_cool():
-    #     return "Be cool, man, be coooool! You're almost a FSND grad!"
+    @app.route("/actors", methods=["GET"])
+    def retrieve_actors():
+        try:
+            selection = Actor.query.order_by(Actor.id).all()
+            current_actors = paginate_actors(request, selection)
+
+            if len(current_actors) == 0:
+                abort(404)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "actors": current_actors,
+                    "total_actors": len(Actor.query.all()),
+                }
+            )
+        except:
+            abort(404)
+
+    # @app.route("/actors/<int:actor_id>", methods=["GET"])
+    # def get_specific_actor(actor_id):
+
+    #     try:
+
+    #         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+    #         current_actors = paginate_actors(request, selection)
+
+    #         if len(current_actors) == 0:
+    #             abort(404)
+
+    #         return jsonify(
+    #             {
+    #                 "success": True,
+    #                 "actors": current_actors,
+    #                 "total_actors": len(current_actors),
+    #             }
+    #         )
+
+    #     except:
+    #         abort(404)
+
+    # 404
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "Resource Not Found"}),
+            404,
+        )
+
+    # 405
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": 405,
+                    "message": "The method is not allowed for the requested URL",
+                }
+            ),
+            405,
+        )
 
     return app
 
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run()
-
+    # pip freeze > requirements.txt
     # FLASK_APP=app.py FLASK_DEBUG=true flask run
+    # pg_ctl -D "C:/Program Files/PostgreSQL/13/data" start
---
+++ models.py
@@ -0,0 +1,61 @@
+import os
+from sqlalchemy import Column, String, Integer, Boolean, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+database_name = "capstonedb"
+database_path = "postgresql://{}:{}@{}/{}".format(
+    "postgres", "7749", "localhost:5432", database_name
+)
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Actor(db.Model):
+    __tablename__ = "actors"
+
+    id = Column(Integer, primary_key=True)
+    attributes_name = Column(String())
+    age = Column(String())
+    gender = Column(String())
+
+
+def __init__(self, attributes_name, age, gender):
+    self.attributes_name = attributes_name
+    self.age = age
+    self.gender = gender
+
+
+def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+
+def update(self):
+    db.session.commit()
+
+
+def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+
+def format(self):
+    return {
+        "id": self.id,
+        "attributes_name": self.attributes_name,
+        "age": self.age,
+        "gender": self.gender,
+    }
+
+
+# movies
