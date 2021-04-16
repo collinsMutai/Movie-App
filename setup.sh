3b05ac153bf07748946fb8afc14e7b5eb179f918
Collins Mutai <collinsfrontend@gmail.com>
2021-04-16 22:43:16 +0300

    update heroku postgres url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL="postgres://postgres:7749@localhost:5432/capstonedb"
+export DATABASE_URL="postgres://msyfkfirrgsedj:6eb27aec9fd35629ae8c42d96993110d93ce50c4a04c8d8604f4f8067d12e65f@ec2-3-234-85-177.compute-1.amazonaws.com:5432/dbt2v8tu6dbdas"
 export AUTH0_DOMAIN="collinsfrontend.us.auth0.com"
 export ALGORITHMS=['RS256']
 export API_AUDIENCE="capstone"
